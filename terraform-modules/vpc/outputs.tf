36a4a0e83a03a23cb5f15af6d5d11e8126daf876
siva <siva@example.com>
2025-08-27 20:46:08 +0530

    added output.tf values

--- terraform-modules/vpc/outputs.tf
+++ terraform-modules/vpc/outputs.tf
@@ -3,12 +3,23 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "private_subnet_ids" {
-  description = "Private subnet IDs"
-  value       = aws_subnet.private[*].id
+# output "private_subnet_ids" {
+#   description = "Private subnet IDs"
+#   value       = aws_subnet.private[*].id
+# }
+
+# output "public_subnet_ids" {
+#   description = "Public subnet IDs"
+#   value       = aws_subnet.public[*].id
+# }
+
+
+output "public_subnets" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
 }
 
-output "public_subnet_ids" {
-  description = "Public subnet IDs"
-  value       = aws_subnet.public[*].id
+output "private_subnets" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
 }
