2395c4053a33d50f73e288582047cf61b274bf05
siva <siva@example.com>
2025-10-10 23:11:34 +0530

    changes made in rds

--- terraform-modules/rds/main.tf
+++ terraform-modules/rds/main.tf
@@ -9,20 +9,19 @@ resource "aws_db_subnet_group" "this" {
 }
 
 
-
 resource "aws_db_instance" "this" {
-  identifier              = "${var.project_name}-mysql-db"
-  allocated_storage       = var.allocated_storage
-  engine                  = var.engine
-  engine_version          = var.engine_version
-  instance_class          = var.instance_class
-  username                = var.db_username
-  password                = var.db_password
-  db_subnet_group_name    = var.db_subnet_group_name
-  vpc_security_group_ids  = var.vpc_security_group_ids
-  multi_az                = var.multi_az
-  storage_type            = var.storage_type
-  publicly_accessible     = var.publicly_accessible
+  identifier             = var.db_instance_identifier != null ? var.db_instance_identifier : "${var.project_name}-mysql-db"
+  allocated_storage      = var.allocated_storage
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  multi_az               = var.multi_az
+  storage_type           = var.storage_type
+  publicly_accessible    = var.publicly_accessible
   backup_retention_period = var.backup_retention_period
 
   tags = {
--- terraform-modules/rds/variables.tf
+++ terraform-modules/rds/variables.tf
@@ -1,65 +1,66 @@
 variable "project_name" {
-  type        = string
-  description = "Project name for tagging and naming"
+  type = string
 }
 
 variable "db_subnets" {
-  type        = list(string)
-  description = "List of private subnet IDs for the DB subnet group"
-}
-
-
-variable "db_subnet_group_name" {
-  type        = string
+  type = list(string)
 }
 
 variable "db_username" {
-  type        = string
+  type = string
 }
 
 variable "db_password" {
-  type        = string
-  sensitive   = true
+  type = string
+  sensitive = true
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+  default = []
 }
 
 variable "engine" {
   type = string
+  default = "mysql"
 }
 
 variable "engine_version" {
   type = string
+  default = "8.0.28"
 }
 
 variable "instance_class" {
   type = string
+  default = "db.t4g.micro"
 }
 
 variable "allocated_storage" {
-  type    = number
+  type = number
   default = 20
 }
 
 variable "multi_az" {
-  type    = bool
+  type = bool
   default = false
 }
 
 variable "storage_type" {
-  type    = string
+  type = string
   default = "gp2"
 }
 
 variable "publicly_accessible" {
-  type    = bool
+  type = bool
   default = false
 }
 
 variable "backup_retention_period" {
-  type    = number
+  type = number
   default = 7
 }
 
-variable "vpc_security_group_ids" {
-  type    = list(string)
-  default = []
+variable "db_instance_identifier" {
+  type = string
+  default = null
 }
