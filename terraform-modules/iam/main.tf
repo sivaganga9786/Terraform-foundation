01b3b45c84e77d5f6626dc4f6f4f0feabc72505c
siva <siva@example.com>
2025-10-10 19:13:21 +0530

    added new changes in iam

--- terraform-modules/iam/main.tf
+++ terraform-modules/iam/main.tf
@@ -1,85 +1,31 @@
-# # Create IAM role
-# resource "aws_iam_role" "this" {
-#   name               = var.role_name
-#   assume_role_policy = var.assume_role_policy
-# }
-
-# # Attach AWS managed policies
-# resource "aws_iam_role_policy_attachment" "managed" {
-#   for_each   = toset(var.managed_policy_arns)
-#   role       = aws_iam_role.this.name
-#   policy_arn = each.value
-# }
-
-# # Default "full access" inline policy covering all AWS services
-# resource "aws_iam_role_policy" "full_access_inline" {
-#   count  = var.full_access_default ? 1 : 0
-#   name   = "${var.role_name}-full-access"
-#   role   = aws_iam_role.this.name
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect   = "Allow"
-#         Action   = "*"
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
-
-# # Extra inline policies per project
-# resource "aws_iam_role_policy" "extra_inline" {
-#   for_each = var.extra_inline_policies
-#   name     = each.key
-#   role     = aws_iam_role.this.name
-#   policy   = each.value
-# }
-
-# # Create instance profile
-# resource "aws_iam_instance_profile" "this" {
-#   name = "${var.role_name}-instance-profile"
-#   role = aws_iam_role.this.name
-# }
-
-
-# -------------------------------
-# Create IAM Role
-# -------------------------------
+# Create IAM role
 resource "aws_iam_role" "this" {
   name               = var.role_name
   assume_role_policy = var.assume_role_policy
 }
 
-# -------------------------------
-# Attach AWS Managed Policies
-# -------------------------------
+# Attach AWS managed policies
 resource "aws_iam_role_policy_attachment" "managed" {
   for_each   = toset(var.managed_policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = each.value
 }
 
-# -------------------------------
-# Create Custom Managed Policies
-# -------------------------------
+# Create custom managed policies
 resource "aws_iam_policy" "custom" {
   for_each   = var.custom_managed_policies
   name       = each.key
   policy     = each.value
-  description = "Custom managed policy created via reusable module"
 }
 
 # Attach custom managed policies
 resource "aws_iam_role_policy_attachment" "custom_attach" {
-  for_each   = aws_iam_policy.custom
+  for_each   = var.custom_managed_policies
   role       = aws_iam_role.this.name
-  policy_arn = each.value.arn
+  policy_arn = aws_iam_policy.custom[each.key].arn
 }
 
-# -------------------------------
-# Extra Inline Policies
-# -------------------------------
+# Extra inline policies per project
 resource "aws_iam_role_policy" "extra_inline" {
   for_each = var.extra_inline_policies
   name     = each.key
@@ -87,9 +33,7 @@ resource "aws_iam_role_policy" "extra_inline" {
   policy   = each.value
 }
 
-# -------------------------------
-# Create Instance Profile for EC2
-# -------------------------------
+# Create instance profile for EC2
 resource "aws_iam_instance_profile" "this" {
   name = "${var.role_name}-instance-profile"
   role = aws_iam_role.this.name
--- terraform-modules/iam/variables.tf
+++ terraform-modules/iam/variables.tf
@@ -1,27 +1,27 @@
 variable "role_name" {
   type        = string
-  description = "Name of the IAM role"
+  description = "Name of the IAM Role"
 }
 
 variable "assume_role_policy" {
   type        = string
-  description = "IAM assume role policy in JSON format"
+  description = "Assume role policy JSON"
 }
 
 variable "managed_policy_arns" {
-  type        = list(string)
-  default     = []
-  description = "List of managed policy ARNs to attach to the role"
+  type    = list(string)
+  default = []
+  description = "List of AWS managed policy ARNs"
 }
 
 variable "custom_managed_policies" {
-  type        = map(string)
-  default     = {}
-  description = "Custom managed policies to create and attach (policy_name => JSON document)"
+  type    = map(string)
+  default = {}
+  description = "Map of custom managed policies: name = JSON document"
 }
 
 variable "extra_inline_policies" {
-  type        = map(string)
-  default     = {}
-  description = "Inline policies to attach to the role (policy_name => JSON document)"
+  type    = map(string)
+  default = {}
+  description = "Extra inline policies"
 }
