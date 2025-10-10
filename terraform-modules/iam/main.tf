3a8746ae89c614500d8408fbba5e6a911af70583
siva <siva@example.com>
2025-10-10 19:03:59 +0530

    added changed code

--- terraform-modules/iam/main.tf
+++ terraform-modules/iam/main.tf
@@ -43,25 +43,43 @@
 # }
 
 
-resource "aws_iam_policy" "this" {
-  name        = var.policy_name
-  description = var.policy_description
-  policy      = var.policy_document
-}
-
+# -------------------------------
+# Create IAM Role
+# -------------------------------
 resource "aws_iam_role" "this" {
   name               = var.role_name
   assume_role_policy = var.assume_role_policy
 }
 
-# Attach AWS managed policies (including custom managed policies)
+# -------------------------------
+# Attach AWS Managed Policies
+# -------------------------------
 resource "aws_iam_role_policy_attachment" "managed" {
   for_each   = toset(var.managed_policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = each.value
 }
 
-# Extra inline policies per project
+# -------------------------------
+# Create Custom Managed Policies
+# -------------------------------
+resource "aws_iam_policy" "custom" {
+  for_each   = var.custom_managed_policies
+  name       = each.key
+  policy     = each.value
+  description = "Custom managed policy created via reusable module"
+}
+
+# Attach custom managed policies
+resource "aws_iam_role_policy_attachment" "custom_attach" {
+  for_each   = aws_iam_policy.custom
+  role       = aws_iam_role.this.name
+  policy_arn = each.value.arn
+}
+
+# -------------------------------
+# Extra Inline Policies
+# -------------------------------
 resource "aws_iam_role_policy" "extra_inline" {
   for_each = var.extra_inline_policies
   name     = each.key
@@ -69,7 +87,9 @@ resource "aws_iam_role_policy" "extra_inline" {
   policy   = each.value
 }
 
-# Create instance profile for EC2
+# -------------------------------
+# Create Instance Profile for EC2
+# -------------------------------
 resource "aws_iam_instance_profile" "this" {
   name = "${var.role_name}-instance-profile"
   role = aws_iam_role.this.name
--- terraform-modules/iam/variables.tf
+++ terraform-modules/iam/variables.tf
@@ -1,32 +1,27 @@
-variable "policy_name" {
-  type        = string
-  description = "Name of the custom managed policy"
-}
-
-variable "policy_description" {
-  type        = string
-  default     = "Custom managed policy"
-}
-
-variable "policy_document" {
-  type        = string
-  description = "JSON policy document"
-}
-
 variable "role_name" {
-  type = string
+  type        = string
+  description = "Name of the IAM role"
 }
 
 variable "assume_role_policy" {
-  type = string
+  type        = string
+  description = "IAM assume role policy in JSON format"
 }
 
 variable "managed_policy_arns" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
+  description = "List of managed policy ARNs to attach to the role"
+}
+
+variable "custom_managed_policies" {
+  type        = map(string)
+  default     = {}
+  description = "Custom managed policies to create and attach (policy_name => JSON document)"
 }
 
 variable "extra_inline_policies" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
+  description = "Inline policies to attach to the role (policy_name => JSON document)"
 }
