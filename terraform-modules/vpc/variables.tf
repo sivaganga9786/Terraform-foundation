1766aaa53f3d49eef483ac9e4dabd82b7979d952
sivaganga9786 <95874425+sivaganga9786@users.noreply.github.com>
2025-10-10 13:54:47 +0530

    Update variables.tf

--- terraform-modules/vpc/variables.tf
+++ terraform-modules/vpc/variables.tf
@@ -17,8 +17,7 @@ variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
 }
-
-#variable "cluster_name" {
-#  description = "Name of the EKS cluster"
-#  type        = string
-#}
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
