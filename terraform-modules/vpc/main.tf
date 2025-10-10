e936f4234c84c7a095416d5860d7d256257de3dc
sivaganga9786 <95874425+sivaganga9786@users.noreply.github.com>
2025-10-10 14:09:23 +0530

    Update main.tf

--- terraform-modules/vpc/main.tf
+++ terraform-modules/vpc/main.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
+  #availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
   availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
 
   # tags = {
@@ -26,7 +27,7 @@ resource "aws_subnet" "public" {
   count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
 
   map_public_ip_on_launch = true
 
